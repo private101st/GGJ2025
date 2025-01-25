7391324f38ba5bb959a509062cd97242071bce24
mattefwe <matterisco@gmail.com>
2025-01-25 16:34:53 +0100

    - mofica 55

--- game jam 2025/scenes/title_screen.gd
+++ game jam 2025/scenes/title_screen.gd
@@ -2,10 +2,10 @@ extends Control
 
 
 func _on_play_button_pressed() -> void:
-	get_tree().change_scene("res://scenes/in_game.tscn")
+	get_tree().change_scene_to_file("res://scenes/in_game.tscn")
 
 func _on_options_button_pressed() -> void:
 	pass # Replace with function body.
 
 func _on_quit_button_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().quit()
